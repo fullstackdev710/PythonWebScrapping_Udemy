a491f94c190b62dcf63a92dbc0c5b4ba360a0d1d
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-24 21:43:21 -0400

    Scraping Multiple Links within the same page

--- bs_basic.py
+++ bs_basic.py
@@ -1,16 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = "https://subslikescript.com/movie/Titanic-120338"
+root = "https://subslikescript.com"
+website = f"{root}/movies"
 result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
 
 box = soup.find('article',class_='main-article')
-title = box.find('h1').get_text()
-transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
-with open(f'{title}.txt','w', encoding="utf-8") as file:
-   file.write(title)
-   file.write(transcript)
+links = []
+for link in box.find_all('a', href=True):
+   links.append(link['href'])
+
+invalid = '<>:"/\|?*'
+
+for link in links:
+   website = f"{root}/{link}"
+   result = requests.get(website)
+   content = result.text
+   soup = BeautifulSoup(content, 'lxml')
+   box = soup.find('article',class_='main-article')
+   title = box.find('h1').get_text(strip=True, separator=' ')
+   filename=title
+   for char in invalid:
+      filename = filename.replace(char, '-')
+
+   print(title)
+   print(filename)
+
+   transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+   with open(f'movies/{filename}.txt','w', encoding="utf-8") as file:
+      file.write(title)
+      file.write(transcript)
+
