4c1437427a396fae69535f99d6e1e881dd01381b
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-24 20:12:54 -0400

    How to Scrape a Single Page & Exporting Data to a TXT file

--- bs_basic.py
+++ bs_basic.py
@@ -6,4 +6,11 @@ result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
+
+box = soup.find('article',class_='main-article')
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+with open(f'{title}.txt','w', encoding="utf-8") as file:
+   file.write(title)
+   file.write(transcript)
