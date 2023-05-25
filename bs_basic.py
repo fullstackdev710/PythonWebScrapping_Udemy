c5a66889a36097f76629c981b289b5c31c06f8b1
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-25 09:31:29 -0400

    Pagination scrapping

--- bs_basic.py
+++ bs_basic.py
@@ -2,37 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 
 root = "https://subslikescript.com"
-website = f"{root}/movies"
+website = f"{root}/movies_letter-A"
 result = requests.get(website)
 content = result.text
-
 soup = BeautifulSoup(content, 'lxml')
 
-box = soup.find('article',class_='main-article')
+pagination = soup.find('ul', class_='pagination')
+pages = pagination.find_all('li', class_='page-item')
+last_page = pages[-2].text
 
-links = []
-for link in box.find_all('a', href=True):
-   links.append(link['href'])
-
-invalid = '<>:"/\|?*'
-
-for link in links:
-   website = f"{root}/{link}"
+for page in range(1, int(last_page)+1):
+   website = f"{root}/movies_letter-A?page={page}"
    result = requests.get(website)
    content = result.text
    soup = BeautifulSoup(content, 'lxml')
+
    box = soup.find('article',class_='main-article')
-   title = box.find('h1').get_text(strip=True, separator=' ')
-   filename=title
-   for char in invalid:
-      filename = filename.replace(char, '-')
 
-   print(title)
-   print(filename)
+   links = []
+   for link in box.find_all('a', href=True):
+      links.append(link['href'])
 
-   transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+   invalid = '<>:"/\|?*'
 
-   with open(f'movies/{filename}.txt','w', encoding="utf-8") as file:
-      file.write(title)
-      file.write(transcript)
+   for link in links:
+      try:
+         website = f"{root}/{link}"
+         result = requests.get(website)
+         content = result.text
+         soup = BeautifulSoup(content, 'lxml')
+         box = soup.find('article',class_='main-article')
+         title = box.find('h1').get_text(strip=True, separator=' ')
+         filename = title
+
+         for char in invalid:
+            filename = filename.replace(char, '-')
+
+         transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+      except:
+         print('-------Link not working ----------------')
+         print(link)
+
+      with open(f'movies/{filename}.txt','w', encoding="utf-8") as file:
+         file.write(title)
+         file.write(transcript)
 
