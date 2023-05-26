666e5c571b23d48ce52de5ee94cb72d4279cb427
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-26 15:37:29 -0400

    scrayp scrapping with multiple links

--- spider_tutorial/spider_tutorial/spiders/worldometer.py
+++ spider_tutorial/spider_tutorial/spiders/worldometer.py
@@ -7,11 +7,31 @@ class WorldometerSpider(scrapy.Spider):
     start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
+        # title = response.xpath('//h1/text()').get()
+        countries = response.xpath('//td/a')
 
-        yield {
-            'titles': title,
-            'countries': countries,
-        }
-        pass
+        for country in countries:
+            country_name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            # absolute url
+            # absolute_url = f'https://www.worldometers.info/{link}'
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
+
+            # relative url
+            yield response.follow(url=link, callback=self.parse_country, meta={'country': country_name})
+
+    def parse_country(self, response):
+        country = response.request.meta['country']
+        rows = response.xpath('(//table[contains(@class, "table")])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'country': country,
+                'year': year,
+                'population': population,
+            }
+        # pass
