eb025e400172735897ffd2954a5fbbb2e638f5a8
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-25 21:47:27 -0400

    Project #1 Dealing with Multiple Pages using Selenium (Amazon Audible Bot): builiding bot

---
+++ amazon_audible_bot.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
+
+website = "https://www.audible.com/search"
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(website)
+driver.maximize_window()
+
+container = driver.find_element('class name', 'adbl-impression-container')
+products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
+
+book_title = []
+book_author = []
+book_length = []
+
+for product in products:
+   print(product.text)
+   print(product.find_element('xpath', './/h3[contains(@class, "bc-heading")]').text)
+   book_title.append(product.find_element('xpath', './/h3[contains(@class, "bc-heading")]').text)
+   book_author.append(product.find_element('xpath', './/li[contains(@class, "authorLabel")]').text)
+   book_length.append(product.find_element('xpath', './/li[contains(@class, "runtimeLabel")]').text)
+
+driver.quit()
+
+df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
+df_books.to_csv('books.csv', index=False)
